9eea7bfab04ef391ccba10193994f458089ed7c3
Tomasz Rojek <tomek@rojek.cc>
2015-12-16 07:51:47 +0100

    desirable method scaffold

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ from election_control_algorithm import PluralityControl
 
 pluralitycontrol = PluralityControl()
 print pluralitycontrol.ccdc()
+
+
+"""
+def method (number_of_voters, number_of_candidates, distribution_model, voting_system, control_type)
+controlable
+return controlable (0 or 1)
+"""
